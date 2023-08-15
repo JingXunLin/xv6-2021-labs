5c33d903280b4950d9b2716cfecd7546e745ece4
JingXunLin <jingxun1110@gmail.com>
2023-08-15 17:24:28 +0800

    Revert "bad modify"
    This reverts commit ef365d6683756ffa1dc711e5a051ea443492e006.

--- kernel/defs.h
+++ kernel/defs.h
@@ -63,10 +63,6 @@ void            ramdiskrw(struct buf*);
 void*           kalloc(void);
 void            kfree(void *);
 void            kinit(void);
-void			add_pg_cnt(uint64 , int );
-void			set_pg_cnt(uint64 , int );
-int				get_pg_cnt(uint64);
-
 
 // log.c
 void            initlog(int, struct superblock*);
@@ -174,7 +170,6 @@ uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
-int				cow(pagetable_t, uint64);
 
 // plic.c
 void            plicinit(void);
